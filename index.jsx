5c1b716643e98d962db7933213d034e24f3b52e1
Serdar Şarbak <serdarsarbak@gmail.com>
2023-11-28 15:45:30 -0500

    install change

--- index.jsx
+++ index.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import About from "./pages/About"
+
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-        </nav>
-      </header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1>quiz app v2</h1>
+   
+    </>
   )
 }
 
